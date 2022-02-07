8ae11fb948d50348d9127e19808c576707611ceb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-07 21:00:47 +0800

    Delete make.sh

--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-g++-4.8 -I/home/chrisdv/libraries/boost_1_54_0 \
--L/home/chrisdv/libraries/boost_1_54_0/stage/lib \
--std=c++11 -l boost_system -l boost_filesystem -l boost_iostreams -O2 -march=native -mtune=native -o cluster_spam cluster_spam.cpp
